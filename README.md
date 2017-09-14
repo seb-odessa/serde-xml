846d0e0e3bfb2b259bb64f3d6648b3d0e9bdd758
seb-odessa <seb@ukr.net>
2017-09-14 22:46:20 +0300

    Play with FB2 format

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # serde-xml
+
+```
+$ ./target/debug/serde-xml < project.xml 
+Project {
+    name: "my_project",
+    items: [
+        Item {
+            name: "hello",
+            source: "world.rs"
+        }
+    ]
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,68 @@
 #[macro_use] extern crate serde_derive;
 extern crate serde_xml_rs;
 use std::io::{self, Read};
-
 use serde_xml_rs::deserialize;
 
-// <Project name="my_project">
-//     <Item name="hello" source="world.rs" />
-// </Project>
+
 #[derive(Debug, Deserialize)]
-struct Item {
-    pub name: String,
-    pub source: String
+pub struct Sequence {
+    name: String,
+    #[serde(default)]
+    number: String,
+    #[serde(rename = "xml:lang", default)]
+    lang: String,
 }
 
 #[derive(Debug, Deserialize)]
-struct Project {
-    pub name: String,
+pub struct Author {
+     #[serde(rename = "first-name", default)]
+     first_name: String,
+     #[serde(rename = "middle-name", default)]
+     middle_name: String,
+     #[serde(rename = "last-name", default)]
+     last_name: String,
+     #[serde(rename = "nickname", default)]
+     nick_name: String,
+     #[serde(rename = "home-page", default)]
+     home_page: String,
+     #[serde(rename = "email", default)]
+     email: String,
+}
 
-    #[serde(rename = "Item", default)]
-    pub items: Vec<Item>
+#[derive(Debug, Deserialize)]
+pub struct TitleInfo {
+//     genre: Vec<Genre>,
+    #[serde(rename = "author", default)]
+    author: Vec<Author>,
+    #[serde(rename = "book-title", default)]
+    book_title: String,
+//     // annotation: OptionalAnnotation,
+//     // keywords
+//     // date
+//     //  coverpage
+    #[serde(rename = "lang", default)]
+    lang: String,
+    #[serde(rename = "src-lang", default)]
+    src_lang: String,
+    #[serde(rename = "translator", default)]
+    translator: Vec<Author>,
+    #[serde(rename = "sequence", default)]
+    sequence: Vec<Sequence>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Description {
+    #[serde(rename = "title-info")]
+    title_info: TitleInfo,
+    //src_title_info: OptionalTitleInfo,
+    //document_info: DocumentInfo,
+    //publish_info: Vec<PublishInfo>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct FictionBook {
+    #[serde(rename = "description")]
+    description: Description,
 }
 
 fn main() {
@@ -26,8 +70,8 @@ fn main() {
     let mut xml = String::new();
     match reader.read_to_string(&mut xml) {
         Ok(_) => {
-            let project: Project = deserialize(xml.as_bytes()).unwrap();
-            println!("{:#?}", project);
+            let obj: FictionBook = deserialize(xml.as_bytes()).unwrap();
+            println!("{:#?}", obj);
         },
         Err(_) => {}
     }
